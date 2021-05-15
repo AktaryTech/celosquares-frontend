ed88eeb0db6b43e151a67b2c683ab49c981bdd99
Adam K <adam@kecskes.net>
2021-05-15 15:45:06 -0500

    Starting over

--- utils/createAccount.js
+++
@@ -1,8 +0,0 @@
-const getAccount = require('./getAccount').getAccount
-
-async function awaitWrapper(){
-    let account = await getAccount()
-    console.log(`Account address: ${account.address}`)
-}
-
-awaitWrapper()
