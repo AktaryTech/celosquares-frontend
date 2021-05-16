7cf82062e30ddd732946694a01b461f719900d82
Adam K <adam@kecskes.net>
2021-05-16 05:03:17 -0500

    Got the grid working

--- pages/gameGrid.js
+++ pages/gameGrid.js
@@ -1,10 +1,13 @@
 import React, { useState, useCallback, useEffect } from "react";
 import AppNavigation from "../components/AppNavigation";
 import TheBox from "../components/TheBox";
-import styles from "../styles/GameGrid.module.css"
+import styles from "../styles/GameGrid.module.css";
 
 export default function GameGrid() {
-  const gridItemValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  let gridItemValues  = []
+  for (let i = 1; i <= 100; i++) {
+    gridItemValues.push(i);
+  }
   const gridItems = gridItemValues.map((gridItem, index) => (
     <TheBox color="orange" className="w-24 py-2 px-3" key={index}>
       {gridItem}
@@ -14,7 +17,14 @@ export default function GameGrid() {
     <>
       <div className="container">
         <AppNavigation />
-        <div class="grid_container">{gridItems}</div>
+        
+        {/* <div class="grid grid-flow-col grid-cols-10 grid-rows-10 gap-4"> */}
+        <div class={styles.grid_container}>
+        
+        {gridItems}
+        
+        </div>
+    
       </div>
     </>
   );
